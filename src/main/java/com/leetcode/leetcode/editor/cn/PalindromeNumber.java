19b3c52eab1f5d30d58941e22a301a1bf2d05c79
chenjun <825766568@qq.com>
2020-09-05 11:54:36 +0800

    add "判断数字是否是回文数"

---
+++ src/main/java/com/leetcode/leetcode/editor/cn/PalindromeNumber.java
@@ -0,0 +1,70 @@
+
+//判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
+//
+// 示例 1: 
+//
+// 输入: 121
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: -121
+//输出: false
+//解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+// 
+//
+// 示例 3: 
+//
+// 输入: 10
+//输出: false
+//解释: 从右向左读, 为 01 。因此它不是一个回文数。
+// 
+//
+// 进阶: 
+//
+// 你能不将整数转为字符串来解决这个问题吗？ 
+// Related Topics 数学 
+// 👍 1212 👎 0
+
+
+package com.leetcode.leetcode.editor.cn;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        Solution solution = new PalindromeNumber().new Solution();
+        boolean s = solution.reverse(12345);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public boolean reverse(int x) {
+
+            // 特殊情况：
+            // 如上所述，当 x < 0 时，x 不是回文数。
+            // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+            // 则其第一位数字也应该是 0
+            // 只有 0 满足这一属性
+            if (x < 0 || (x % 10 == 0 && x != 0)) {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            while (x > revertedNumber) {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+            // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+            // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+            return x == revertedNumber || x == revertedNumber / 10;
+
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
