e42a018dda3ebb55013aea602a50d5a132223bb4
chenjun <825766568@qq.com>
2020-09-10 16:41:56 +0800

    add "组合总和"

--- src/main/java/com/leetcode/leetcode/editor/cn/CombinationSum.java
+++ src/main/java/com/leetcode/leetcode/editor/cn/CombinationSum.java
@@ -46,20 +46,48 @@
 
 package com.leetcode.leetcode.editor.cn;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CombinationSum{
+public class CombinationSum {
 
-public static void main(String[] args) {
-       Solution solution = new CombinationSum().new Solution();
+    public static void main(String[] args) {
+        Solution solution = new CombinationSum().new Solution();
+        int[] candidates = {2,3,4,5,6};
+        System.out.println(solution.combinationSum(candidates,6));
     }
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        return null;
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> ans = new ArrayList<List<Integer>>();
+            List<Integer> combine = new ArrayList<Integer>();
+            dfs(candidates, target, ans, combine, 0);
+            return ans;
+        }
+
+        public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+            if (idx == candidates.length) {
+                return;
+            }
+            if (target == 0) {
+                ans.add(new ArrayList<Integer>(combine));
+                return;
+            }
+            // 选择当前数
+            if (target - candidates[idx] >= 0) {
+                combine.add(candidates[idx]);
+                dfs(candidates, target - candidates[idx], ans, combine, idx);
+                combine.remove(combine.size() - 1);
+            }
+            // 直接跳过
+            dfs(candidates, target, ans, combine, idx + 1);
+
+        }
+
+
+
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
