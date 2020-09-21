bc491394e17d2a6e9a25ab3c52093a08b792f33d
chenjun <825766568@qq.com>
2020-09-21 09:16:34 +0800

    add "2. 两数相加 "

---
+++ src/main/java/com/leetcode/leetcode/editor/support/ListNode.java
@@ -0,0 +1,14 @@
+package com.leetcode.leetcode.editor.support;
+
+/**
+ * @ClassName: ListNode
+ * @Description:
+ * @Author Chenjun
+ * @Date 2020/09/11 16:47
+ * @Version V1.0
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;   // 下一个链表对象
+    public ListNode(int x) { val = x; }  //赋值链表的值
+}
